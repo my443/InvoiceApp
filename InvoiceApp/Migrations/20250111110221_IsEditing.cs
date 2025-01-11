052b6d9fc2efa3ffc0a814ce4a1d03c4d27d7757
John van Dijk <john@johnv.ca>
2025-01-11 06:17:00 -0500

    Start of day commit.

---
+++ InvoiceApp/Migrations/20250111110221_IsEditing.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsEditing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEditing",
+                table: "Expenses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEditing",
+                table: "Expenses");
+        }
+    }
+}

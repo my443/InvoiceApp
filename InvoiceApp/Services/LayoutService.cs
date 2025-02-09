cf97ee157f9bc87aa3b7bfafe5a27681adf59666
John van Dijk <john@johnv.ca>
2025-02-08 21:41:42 -0500

    Nav menu is working.

---
+++ InvoiceApp/Services/LayoutService.cs
@@ -0,0 +1,15 @@
+﻿namespace InvoiceApp.Services
+{
+    public class LayoutService
+    {
+        public int sidebarWidth { get; set; } = 270;
+
+        public event Action OnSidebarWidthChanged; // Add an event
+
+        public void SetSidebarWidth(int width)
+        {
+            sidebarWidth = width;
+            OnSidebarWidthChanged?.Invoke(); // Trigger the event
+        }
+    }
+}

7adc224d81556e1b0f2c91064f4a7dc3866c38bb
John van Dijk <john@johnv.ca>
2025-02-17 10:17:43 -0500

    Updates to the validation code. To make sure that it doesn't just fail when there is no attachment.

--- InvoiceApp/Validators/QuickAddValidator.cs
+++ InvoiceApp/Validators/QuickAddValidator.cs
@@ -22,24 +22,34 @@ namespace InvoiceApp.Validators
             {
                 ErrorMessages.Add("Total Amount must be greater than 0");
                 isValid = false;
-            }            
+            }
             if (NewExpense.Vendor == null || NewExpense.Vendor == "")
             {
                 ErrorMessages.Add("Enter a vendor name, or \"Unknown\"");
                 isValid = false;
             }
-            if (BrowserFile.Name == null || BrowserFile.Name == "")
+
+            isValid = isValid ? CheckFileValidity(BrowserFile) : isValid;   // If isValid is false, return whatever comes from CheckFileValidity. Otherwise return false. 
+
+            IsValid = isValid;
+            return isValid;
+        }
+
+        private bool CheckFileValidity(IBrowserFile BrowserFile)
+        {
+            bool isValid = true;
+
+            if (BrowserFile == null || BrowserFile.Name == null || BrowserFile.Name == "")
             {
                 ErrorMessages.Add("Select an image or document to be uploaded.");
                 isValid = false;
             }
-            if (BrowserFile.Size > 20971520)
+            if (BrowserFile != null && BrowserFile.Size > 20971520)
             {
                 ErrorMessages.Add("File size must be less than 20MB");
                 isValid = false;
             }
 
-            IsValid = isValid;
             return isValid;
         }
 
